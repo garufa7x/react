b2e1ab8683b7fd34f29bf1f7dc531b14859fe03e
Agustín Berstein <garufa7x@hotmail.com>
2023-07-21 14:49:53 -0300

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-    <NavBar />
-    {/* <ItemListContainer /> */}
-    <ItemDetailContainer itemId={7}/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route path='item' element={<ItemDetailContainer itemId={2} />} />
+        </Routes>
+
+
+      </BrowserRouter>
     </div>
   );
 }
