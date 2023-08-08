f2f35348b12cc570d0e41ff75c1214e7945b7fd8
Agustín Berstein <garufa7x@hotmail.com>
2023-08-08 15:00:40 -0300

    ItemCount

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ItemCount = () => {
+  return (
+    <div>
+        <div className="item-count">
+            <button>-</button>
+            <p></p>
+            <button>+</button>
+        </div>
+    </div>
+  )
+}
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom"
+import { ItemCount } from "./ItemCount"
 
 
-export const ItemDetail = ({item}) => {
+export const ItemDetail = ({ item }) => {
   return (
     <div className="container">
-        <div className="producto-detalle"></div>
-        <img src={item.brand} alt="" />
-        <div>
-          <h3 className="titulo">{item.product_name}</h3>
-          <p className="descripcion">{item.descripcion}</p>
-          <p className="categoria">{item.category}</p>
-          <p className="precio">{item.price}</p>
-        </div>
-
+      <div className="producto producto-detalle">
+      <img src={item.brand} alt="" />
+      <div>
+        <h3 className="titulo">{item.product_name}</h3>
+        <p className="descripcion">{item.descripcion}</p>
+        <p className="categoria">{item.category}</p>
+        <p className="precio">{item.price}</p>
+        <ItemCount />
+        <Link className="ver-mas" to={`/productos/`}>Volver</Link>
+      </div>
+      </div>
     </div>
   )
 }
