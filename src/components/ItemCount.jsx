55795f4bddb72ef20d8ba4241dfbd64b58916081
Agustín Berstein <garufa7x@hotmail.com>
2023-08-11 15:25:31 -0300

    ItemDetail_stock

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const ItemCount = () => {
+
+export const ItemCount = ({cantidad, handleRestar, handleSumar , handleAgregar}) => {
+
+
   return (
     <div>
         <div className="item-count">
-            <button>-</button>
-            <p></p>
-            <button>+</button>
+            <button onClick={handleRestar}>-</button>
+            <p>{cantidad}</p>
+            <button onClick={handleSumar}>+</button>            
         </div>
+        <button className='agregar-al-carrito' onClick={handleAgregar}>Agregar al carrito</button>
     </div>
   )
 }
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom"
 import { ItemCount } from "./ItemCount"
+import { useState } from "react";
 
 
 export const ItemDetail = ({ item }) => {
+
+  const [cantidad, setCantidad] =useState(1);
+
+  const handleRestar = () => {
+    cantidad > 1 && setCantidad (cantidad -1)
+  }
+
+  const handleSumar = () => {
+    cantidad < item.quantity && setCantidad(cantidad +1)
+  }
+
+  const handleAgregar = () => {
+    console.log ({...item, cantidad});
+  }
+
   return (
     <div className="container">
       <div className="producto producto-detalle">
@@ -12,7 +28,7 @@ export const ItemDetail = ({ item }) => {
         <p className="descripcion">{item.descripcion}</p>
         <p className="categoria">{item.category}</p>
         <p className="precio">{item.price}</p>
-        <ItemCount />
+        <ItemCount cantidad={cantidad} handleRestar={handleRestar} handleSumar={handleSumar} handleAgregar={handleAgregar} />
         <Link className="ver-mas" to={`/productos/`}>Volver</Link>
       </div>
       </div>
